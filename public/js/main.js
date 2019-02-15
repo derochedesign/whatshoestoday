54d231825ce8cba57590d6e8d893c3de5754ef6b
Aaron <aaron@deroche.design>
2019-02-15 14:54:56 -0500

    Added geolocation, dynamic weather
    Weather report (temp and snow in last 2 hours) is now dynamic to users coordinates.

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,105 @@
 var temp;
+var timezone;
 var dataSet;
+var userPosition = {
+	"lat":"0",
+	"long":"0"
+}
+$("#getCoords").hide();
 
-fetch('/get-weather')
-		.then(res => res.json())
-    .then(data => {
-      dataSet = data;
-			console.log(data);
-      temp = dataSet.data.currently.temperature;
-      $("#test").text("It is "+temp+"ºC in Toronto");
-    });
+var currDate = new Date();
+var currHour = currDate.getHours(); //this is 24hr
+geoFindMe();
+
+function geoFindMe() {
+
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+
+  function success(position) {
+    userPosition.lat = position.coords.latitude;
+    userPosition.long = position.coords.longitude;
+		postReq();
+    console.log('<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>');
+  }
+
+  function error() {
+    console.log("Unable to retrieve your location");
+		$("#getCoords").show();
+  }
+
+  console.log("<p>Locating…</p>");
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+$("#submitCoords").click(function() {
+	var tempPosition = $("#getLat").val()+", "+$("#getLong").val();
+	console.log(tempPosition);
+
+	if (checkValid(tempPosition)){
+		userPosition.lat = $("#getLat").val();
+		userPosition.long = $("#getLong").val();
+		console.log("good");
+		alert("saved location");
+		$("#getCoords").hide();
+		postReq();
+	}
+	else {
+		console.log("failed");
+		alert("incorrect");
+	}
+});
+
+function checkValid(data) {
+	var re = /^(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)$/
+	return re.test(data);
+}
+
+function postReq() {
+	console.log(userPosition);
+	    $.ajax({
+	      type: 'POST',
+	      url: '/search',
+	      data: userPosition,
+	      success: function(data){
+	        //do something with the data via front-end framework
+					console.log("saved location");
+	      }
+	    }); //close ajax
+}
+
+
+
+$("#getTemp").click(function() {
+	fetch('/get-weather')
+			.then(res => res.json())
+	    .then(data => {
+	      dataSet = data;
+				console.log(data);
+	      temp = dataSet.data.currently.temperature;
+				timezone = dataSet.data.timezone;
+	      $("#test").text("It is "+temp+"ºC in "+timezone);
+	    });
+});
+
+$("#getSnowHistory").click(function() {
+	fetch('/get-history')
+			.then(res => res.json())
+	    .then(data => {
+	      dataSet = data;
+				console.log(data);
+				if (dataSet.data.hourly.data[currHour-1].precipType == "snow" || dataSet.data.hourly.data[currHour-2].precipType == "snow") {
+						$("#test").text("It has snowed in the last two hours.");
+				}
+				else if (dataSet.data.hourly.data[currHour-1].precipType == "rain" || dataSet.data.hourly.data[currHour-2].precipType == "rain") {
+						$("#test").text("It has not snowed, but it did rain in the last two hours.");
+				}
+				else {
+					$("#test").text("It has not snowed in the last two hours.");
+				}
+
+	    });
+});
--- weather.js
+++ weather.js
@@ -1,19 +1,26 @@
 const fetch = require('node-fetch');
+var bodyParser = require("body-parser");
+
+var urlencode = bodyParser.urlencoded({extended: false});
+var position;
 
 module.exports = (app) => {
+
+  app.post('/search', urlencode, (req, res) => {
+		position = req.body;
+    res.redirect('/get-weather');
+
+  });
 
   app.get('/get-weather', (req, res) => {
   	//build api URL with user zip
   	const baseUrl = 'https://api.darksky.net/forecast/';
     const appKey = "6d5254f8b0472605fde6c25c5207d6c8";
-    var lat = "42.3601";
-    var long = "-71.0589";
+    var lat = "43.651588";
+    var long = "-79.367270";
+    lat = position.lat;
+    long = position.long;
 
-  	// const userLocation = (url1, url2, zipcode) => {
-  	// 	let newUrl = url1 + zipcode + url2;
-  	// 	return newUrl;
-  	// };
-  	// const apiUrl = userLocation(baseUrl, apiId, zipcode);
     apiUrl = "https://api.darksky.net/forecast/"+appKey+"/"+lat+","+long+"?units=si";
 
   	fetch(apiUrl)
@@ -24,6 +31,32 @@ module.exports = (app) => {
   	.catch(err => {
   		res.redirect('/error');
   	});
+
+  });
+
+  app.get('/get-history', (req, res) => {
+    //build api URL with user zip
+    const baseUrl = 'https://api.darksky.net/forecast/';
+    const appKey = "6d5254f8b0472605fde6c25c5207d6c8";
+    var lat = "43.651588";
+    var long = "-79.367270";
+    lat = position.lat;
+    long = position.long;
+    if (!Date.now) {
+      Date.now = function() { return new Date().getTime(); }
+    }
+    var time = Math.round((new Date()).getTime() / 1000);
+
+    apiUrl = "https://api.darksky.net/forecast/"+appKey+"/"+lat+","+long+","+time+"?units=si&exclude=flags,daily,alerts";
+
+    fetch(apiUrl)
+    .then(res => res.json())
+    .then(data => {
+      res.send({ data });
+    })
+    .catch(err => {
+      res.redirect('/error');
+    });
 
   });
 }
